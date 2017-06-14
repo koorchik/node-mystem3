7a923adb5e031309ed605f9c3e586066b1a1aeb8
bulgakovk <kbulgakov@yahoo.com>
2017-06-14 18:57:24 +0300

    Tests for fetchAllGrammemes added

--- tests/stem.js
+++ tests/stem.js
@@ -38,3 +38,42 @@ test('Lemmatize non word', function(done) {
         done();
     });
 });
+
+test('Fetch all grammemes known word', function(done) {
+    var myStem = new MyStem();
+    myStem.start();
+
+    myStem.fetchAllGrammemes("немцы").then(function(lemma) {
+        /*
+        Существительное, мужской род, одушевлённое, именительный падеж, множественное число
+         */
+        assert.deepEqual( lemma, ['немец', 'S', 'm', 'anim=nom', 'pl' ]);
+    }).then(function() {
+        myStem.stop();
+        done();
+    });
+});
+
+test('Fetch all grammemes non word', function(done) {
+    var myStem = new MyStem();
+    myStem.start();
+
+    myStem.fetchAllGrammemes("шоп78шол").then(function(lemma) {
+        assert.equal( lemma, "шоп78шол");
+    }).then(function() {
+        myStem.stop();
+        done();
+    });
+});
+
+test('Fetch all grammemes unknown word', function(done) {
+    var myStem = new MyStem();
+    myStem.start();
+
+    myStem.fetchAllGrammemes("хелоу").then(function(lemma) {
+        assert.equal( lemma[0], "хелоу");
+    }).then(function() {
+        myStem.stop();
+        done();
+    });
+});
